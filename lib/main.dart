eadc4fd26decdf99f7b2969ea8f10b6f76e3665b
recursion corp <contact@recursioncorp.pl>
2020-02-28 13:53:43 +0100

    Categories screen, Category model, dummy data

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('DeliMeals'),
+        title: Text('Yum yum'),
       ),
       body: Center(
         child: Text('Navigation Time!'),
