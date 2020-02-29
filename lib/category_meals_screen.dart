69382aa66fa134326884763861803d6555fe1d19
recursion corp <contact@recursioncorp.pl>
2020-02-29 13:38:49 +0100

    Another page (template for now)

---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('CategoryMealsScreen'),
+      ),
+      body: Center(
+        child: Text('test'),
+      ),
+    );
+  }
+}
