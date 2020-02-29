7801a0446fa98fe2af28cd445141d0a450a9591d
recursion corp <contact@recursioncorp.pl>
2020-02-29 13:08:37 +0100

    Grid of categories

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
+import './dummy_data.dart';
+import './category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: <Widget>[],
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(title: const Text('Meals App'),),
+      body: GridView(
+        children: DUMMY_CATEGORIES.map((item) {
+          return CategoryItem(item.title, item.color);
+        }).toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
       ),
     );
   }
