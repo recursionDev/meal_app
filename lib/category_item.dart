27d591bba7d2bede90e98fc5cd2be484fcf132af
recursiondev <damiangrzegolec@gmail.com>
2020-03-02 17:13:53 +0100

    Named routes

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -8,11 +8,11 @@ class CategoryItem extends StatelessWidget {
 
   CategoryItem(this.id, this.title, this.color);
 
-  void selectCategory(BuildContext ctx)
-  {
-    Navigator.of(ctx).push(MaterialPageRoute(builder: (_) {
-      return CategoryMealsScreen(id, title);
-    }));
+  void selectCategory(BuildContext ctx) {
+    Navigator.of(ctx).pushNamed(CategoryMealsScreen.routeName, arguments: {
+      'id': id,
+      'title': title,
+    });
   }
 
   @override
