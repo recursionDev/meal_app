e44e2eed1351878744b50159b4921589efd29e8d
recursion corp <contact@recursioncorp.pl>
2020-03-06 09:57:38 +0100

    Meal detail route, default route (ala 404 in web)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+
 import './screens/categories_screen.dart';
 import './screens/category_meals_screen.dart';
+import './screens/meal_detail_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -30,7 +32,11 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (ctx) => CategoriesScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
-      }
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+      },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
+      },
     );
   }
 }
---
+++ lib/screens/meal_detail_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MealDetailScreen extends StatelessWidget {
+  static const routeName = '/meal-detail';
+
+  @override
+  Widget build(BuildContext context) {
+    final String id = ModalRoute.of(context).settings.arguments as String;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('$id'),
+      ),
+      body: Center(
+        child: Text('The meal - $id'),
+      ),
+    );
+  }
+}
